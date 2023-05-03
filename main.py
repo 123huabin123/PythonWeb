018d5338a2bf09ad98f25a6ca57d1715ef57bd1b
Hua bin Fu <f1925799271f@gmail.com>
2023-05-03 17:59:48 +0200

    crete list games

--- main.py
+++ main.py
@@ -2,13 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+listGames = [
+    {
+        "name": "Dark souls 3",
+        "price": 49.99,
+        "image_url": "img/darkSouls3.jpg"
+    },
+    {
+        "name": "Doom eternal",
+        "price": 34.99,
+        "image_url": "img/doomEternal.jpg"
+    },
+    {
+        "name": "Hollow Knight: Silksong",
+        "price": 14.99,
+        "image_url": "img/silksong.jpeg"
+    },
+    {
+        "name": "The Legend of Zelda: Tears of The Kingdom",
+        "price": 24.99,
+        "image_url": "img/zelda.jpg"
+    }
+]
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/shopping')
 def shopping():
-    return render_template('shopping.html')
+    return render_template('shopping.html', listGames=listGames)
 
 @app.route('/contact')
 def gaming():
